f118e821046bfc5cc878c191368a0d2def02fd5a
George Len <ghidorah111@gmail.com>
2023-01-05 18:43:30 +0100

    create express app

--- server/server.js
+++ server/server.js
@@ -2,3 +2,17 @@ const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
 require("dotenv").config()
+
+//create express app
+app = express()
+
+app.use(cors())
+app.use(express.json())
+
+const PORT = process.env.PORT || 3003
+
+app.get("/", (req,res) => {
+    res.json("Hello I am working")
+})
+
+app.listen(PORT, () => console.log("Listening on port 3003"))
