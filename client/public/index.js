74bee0e68728a29d8262f4017c7e4bf5276b1e74
George Len <ghidorah111@gmail.com>
2023-01-16 20:42:17 +0100

    reformat displayKriks using template literals

--- client/public/index.js
+++ client/public/index.js
@@ -51,28 +51,21 @@ async function getKrikByName(name) {
 
 function displayKriks(data) {
     krikContainer.innerHTML = ""
+
     if (data.count > 0) {
         data.kriks.forEach((record) => {
-            let krik = document.createElement("div")
-            krik.classList.add("krik")
-            let name = document.createElement("h3")
-            name.classList.add("krik-name")
-            name.textContent = record.name
-            let krikContent = document.createElement("div")
-            krikContent.classList.add("krik-content")
-            let content = document.createElement("p")
-            content.textContent = record.content
-
-            krikContent.appendChild(content)
-            let date = document.createElement("span")
-            date.textContent = new Date(record.createdAt).toDateString()
-            krikContent.appendChild(date)
-
-            krik.appendChild(name)
-            krik.appendChild(krikContent)
-
-            krikContainer.appendChild(krik)
+            //initializing date here as a variable
+            let dateString = new Date(record.createdAt).toDateString()
+            krikContainer.innerHTML += `
+            <div class="krik">
+                <h3 class="krik-name">${record.name}</h3>
+                <div class="krik-content">
+                    <p>${record.content}</p>
+                    <span class="date">${dateString}</span>
+                </div>
+                `
         })
+        return krikContainer
     } else {
         return;
     }
@@ -83,15 +76,15 @@ function displayKriks(data) {
 const showAll = document.querySelector(".show-all")
 const showAllBtn = document.querySelector(".show-all-btn")
 
-document.addEventListener("click", (e)=> {
-    if((e.target.matches("h3") && e.target.getAttribute("class") == "krik-name")) {
+document.addEventListener("click", (e) => {
+    if ((e.target.matches("h3") && e.target.getAttribute("class") == "krik-name")) {
         let name = e.target.textContent
         getKrikByName(name)
         showAll.classList.remove("dn")
     }
 })
 
-showAllBtn.addEventListener("click", ()=> {
+showAllBtn.addEventListener("click", () => {
     getAllKriks()
     showAll.classList.add("dn")
 })
